182303d324e311a5ba222ad1d1a3d87bf6ca292d
chandru <chandrashekharr355@gmail.com>
2023-04-25 09:48:01 +0000

    Authentication And Authorization Part-3

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,59 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getApiResult()
+  }
+
+  getApiResult = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    const dataList = await response.json()
+    const formattedList = dataList.products.map(products => ({
+      title: products.title,
+      id: products.id,
+      brand: products.brand,
+      imageUrl: products.image_url,
+      rating: products.rating,
+      price: products.price,
+    }))
+    this.setState({
+      productsList: formattedList,
+    })
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
